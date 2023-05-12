f7f620d7ebab59607e2427a927b4f716bd93f230
Damir Mirasov <damirmirasovmain@gmail.com>
2023-05-12 22:52:49 +0600

    Change some things in NewsSourceEntity

--- src/main/java/com/github/owakira/news/model/entity/NewsSourceEntity.java
+++ src/main/java/com/github/owakira/news/model/entity/NewsSourceEntity.java
@@ -6,23 +6,27 @@ import lombok.ToString;
 
 import javax.persistence.*;
 
-@Table(name = NewsSourceEntity.TABLE_NAME)
+@Table(
+        name = NewsSourceEntity.TABLE_NAME,
+        schema = NewsSourceEntity.SCHEMA_NAME
+)
 @Entity
 @Getter
 @Setter
 @ToString
 public class NewsSourceEntity {
     public final static String TABLE_NAME = "news_source";
+    public final static String SCHEMA_NAME = "public";
     public final static String ID_COLUMN_NAME = "id";
     public final static String NAME_COLUMN_NAME = "name";
 
+    public final static int NAME_COLUMN_LENGTH = 256;
+
     @Id
     @Column(name = ID_COLUMN_NAME)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = NAME_COLUMN_NAME, nullable = false)
+    @Column(name = NAME_COLUMN_NAME, nullable = false, length = NAME_COLUMN_LENGTH)
     private String name;
-
-
 }
