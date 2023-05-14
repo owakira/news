825c43c758ecd14dcdb3480f95ddf6903dddd222
Damir Mirasov <damirmirasovmain@gmail.com>
2023-05-14 22:14:40 +0600

    Create NewsScheduler

--- src/main/java/com/github/owakira/news/NewsApplication.java
+++ src/main/java/com/github/owakira/news/NewsApplication.java
@@ -2,7 +2,9 @@ package com.github.owakira.news;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class NewsApplication {
     public static void main(String[] args) {
